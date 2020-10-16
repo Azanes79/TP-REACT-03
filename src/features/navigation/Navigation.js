160d4083cd309e57863d3739a657017da5c9412c
Alexis <alexis.lemarchand@ynov.com>
2020-10-16 10:41:21 +0200

    add Responsive on all existing pages

--- src/features/navigation/Navigation.js
+++ src/features/navigation/Navigation.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Nav, Button, Text, Box } from 'grommet'
+import { Nav, Button, Text, Box, ResponsiveContext } from 'grommet'
 import { Send, Technology } from 'grommet-icons'
 import './Navigation.css'
 
 const Navigation = () => {
+    const size = React.useContext(ResponsiveContext);
+
     return (
         <Nav background="#009d55" align="center" direction="row" justify="between" pad="small" gap="small" width="100%">
             <Box gap="10px" direction="row" justify="between" align="center">
-                <Technology></Technology>
                 <Text>Alexis LEMARCHAND</Text>
             </Box>
-            <Button label="Me Contacter" icon={<Send />} active={false} color="#1f461d" />
+            <Technology></Technology>
+            <Button label="Me Contacter" icon={size === "small" ? null : <Send />} active={false} color="#1f461d" />
         </Nav>
     );
 }
