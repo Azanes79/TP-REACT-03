04f203f8ad9a96fa2f1a0a3b92d1df41269e143e
Alexis <alexis.lemarchand@ynov.com>
2020-10-22 08:40:32 +0200

    SAVE

--- src/features/parcour/Parcour.js
+++ src/features/parcour/Parcour.js
@@ -11,54 +11,62 @@ const Parcour = () => {
     const [dataSelect, setDataSelect] = useState(data[0]);
     return (
         <Box fill="horizontal" background="#009d55" height="500px" id="box-parcour">
-            <Stack guidingChild={1} alignSelf="center">
+            <Stack guidingChild={1} alignSelf="center" margin="2vw 0">
                 <Diagram
                     connections={[
                         {
                             fromTarget: '1',
                             toTarget: '2',
                             thickness: 'xsmall',
-                            color: 'accent-2',
+                            color: '#6FFFB0',
                         },
                         {
                             fromTarget: '2',
                             toTarget: '3',
                             thickness: 'xsmall',
-                            color: 'accent-2',
+                            color: '#6FFFB0',
                             type: 'rectilinear',
                         },
                         {
                             fromTarget: '3',
                             toTarget: '4',
                             thickness: 'xsmall',
-                            color: 'accent-2',
+                            color: '#6FFFB0',
                             type: 'rectilinear',
                         }
                     ]}
                 />
                 <Box direction={size === "small" ? "column" : "row"}>
-                    <Box id="1" margin="small" pad="medium" background="light-4" itemID={data[0].id} onClick={() => setDataSelect(data[0])}><Text textAlign="center">{data[0].name}</Text></Box>
-                    <Box id="2" margin="small" pad="medium" background="light-4" itemID={data[1].id} onClick={() => setDataSelect(data[1])}><Text textAlign="center">{data[1].name}</Text></Box>
-                    <Box id="3" margin="small" pad="medium" background="light-4" itemID={data[2].id} onClick={() => setDataSelect(data[2])}><Text textAlign="center">{data[2].name}</Text></Box>
-                    <Box id="4" margin="small" pad="medium" background="light-4" itemID={data[3].id} onClick={() => setDataSelect(data[3])}><Text textAlign="center">{data[3].name}</Text></Box>
+                    <Box id="1" className="box-parcour-diplome" margin="small" pad="medium" width="10vw" background="#254030" itemID={data[0].id} onClick={() => setDataSelect(data[0])}><Text textAlign="center">BAC</Text></Box>
+                    <Box id="2" className="box-parcour-diplome" margin="small" pad="medium" width="10vw" background="#254030" itemID={data[1].id} onClick={() => setDataSelect(data[1])}><Text textAlign="center">BTS</Text></Box>
+                    <Box id="3" className="box-parcour-diplome" margin="small" pad="medium" width="10vw" background="#254030" itemID={data[2].id} onClick={() => setDataSelect(data[2])}><Text textAlign="center">Licence</Text></Box>
+                    <Box id="4" className="box-parcour-diplome" margin="small" pad="medium" width="10vw" background="#254030" itemID={data[3].id} onClick={() => setDataSelect(data[3])}><Text textAlign="center">Mastère</Text></Box>
                 </Box>
             </Stack>
-            <Grid className="grid-parcour" fill="horizontal">
-                <Box>
-                    <Certificate />
-                    <Text>{dataSelect.name}</Text>
+            <Grid className={size === "small" ? "grid-parcour-row" : "grid-parcour-column"}  fill="horizontal">
+                <Box className="box-parcour" align="center" justify="center" background="#254030" pad="10px" width="80%" height="250px">
+                    <Grid className="grid-parcour-detail">
+                        <Certificate className="icon-parcour-detail" />
+                        <Text alignSelf="start" justifySelf="center" size="24px" textAlign="center">{dataSelect.name}</Text>
+                    </Grid>
                 </Box>
-                <Box>
-                    <AppsRounded />
-                    <Text>{dataSelect.description}</Text>
+                <Box className="box-parcour" align="center" justify="center"  background="#254030" pad="10px" width="80%" height="250px">
+                    <Grid className="grid-parcour-detail">
+                        <AppsRounded className="icon-parcour-detail" />
+                        <Text alignSelf="start" justifySelf="center" size="20px" textAlign="center">{dataSelect.description}</Text>
+                    </Grid>
                 </Box>
-                <Box>
-                    <Location />
-                    <Text>{dataSelect.lieu}</Text>
+                <Box className="box-parcour" align="center" justify="center" background="#254030" pad="10px" width="80%" height="250px">
+                    <Grid className="grid-parcour-detail">
+                        <Location className="icon-parcour-detail" />
+                        <Text alignSelf="start" justifySelf="center" size="24px" textAlign="center">{dataSelect.lieu}</Text>
+                    </Grid>
                 </Box>
-                <Box>
-                    <Calendar />
-                    <Text>{dataSelect.date}</Text>
+                <Box className="box-parcour" align="center" justify="center" background="#254030" pad="10px" width="80%" height="250px">
+                    <Grid className="grid-parcour-detail">
+                        <Calendar className="icon-parcour-detail" />
+                        <Text alignSelf="start" justifySelf="center" size="24px" textAlign="center">{dataSelect.date}</Text>
+                    </Grid>
                 </Box>
             </Grid>
         </Box>
